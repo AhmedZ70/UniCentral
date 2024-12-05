efdd9ee95171276d9cb0bdd063a36214484ed639
joecorbeil <josephcorbeil2@gmail.com>
2024-12-05 13:14:39 -0700

    Basic user service class setup

--- backend/uni_central/uni_central/services.py
+++ backend/uni_central/uni_central/services.py
@@ -1,5 +1,6 @@
 from firebase_admin import auth
 from .models import User
+from django.db import IntegrityError
 
 class FirebaseService:
     @staticmethod
@@ -26,3 +27,44 @@ class FirebaseService:
         Delete user from SQL database when deleted from Firebase
         """
         User.objects.filter(firebase_uid=firebase_uid).delete()
+
+class UserService:
+    @staticmethod
+    def create_user(email_address, fname, lname):
+        """
+        Creates a new user in the SQLite database.
+        Args:
+            email_address (str): User's email address.
+            fname (str): User's first name.
+            lname (str): User's last name.
+        Returns:
+            User: The created user object.
+        """
+        try:
+            # Attempt to create a new user
+            user = User.objects.create(
+                email_address=email_address,
+                fname=fname,
+                lname=lname
+            )
+            return user
+        except IntegrityError:
+            # Handle case where the email address already exists
+            return None
+
+    @staticmethod
+    def delete_user(user_id):
+        """
+        Deletes a user from the SQLite database by user_id.
+        Args:
+            user_id (int): The ID of the user to delete.
+        Returns:
+            bool: True if user was deleted, False if user not found.
+        """
+        try:
+            user = User.objects.get(id=user_id)
+            user.delete()
+            return True
+        except User.DoesNotExist:
+            # If user is not found, return False
+            return False
