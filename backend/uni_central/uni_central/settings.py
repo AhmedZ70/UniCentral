ff0ca2c58cb5214e9a8c10c060fa020ddd467e33
Raj Reddy <rajreddy259@gmail.com>
2025-01-28 21:54:42 -0700

    added static root

--- backend/uni_central/uni_central/settings.py
+++ backend/uni_central/uni_central/settings.py
@@ -128,8 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "uni_central", "static"),
-                    os.path.join(BASE_DIR, 'frontend', 'static')]  # Update the path to point to the correct static files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "uni_central", "static"),  
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
